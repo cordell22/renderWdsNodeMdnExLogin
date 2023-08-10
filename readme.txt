2b7dca1e0940d4bc2cc769e29c7f7d32eb669a6f
cordel22 <cordelfenevall@gmail.com>
2023-08-10 16:16:59 +0200

    first commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+builds on local, not on railway
---
+++ server.js
@@ -0,0 +1,116 @@
+/*  test express server
+const express = require('express')
+const app = express()
+
+app.listen(3000)
+*/
+
+
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
+
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+//  app.listen(3000)
+
+const initializePassport = require('./passport-config')
+initializePassport(
+	passport, 
+	email => users.find(user => user.email === email),
+	id => users.find(user => user.id === id),
+)
+
+
+
+
+//	instead of a database
+const users = []
+
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+
+app.get('/', checkAuthenticated, (req, res) => {
+	res.render('index.ejs', { bitchez: req.user.name })
+})
+
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+	res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+	res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+	try {
+		const hashedPassword = await bcrypt.hash(req.body.password, 10)
+		users.push({
+			id: Date.now().toString(),	//	generated automatically in case of database
+			name: req.body.name,
+			email: req.body.email,
+			password: hashedPassword
+		})
+		res.redirect('/login')
+	} catch {
+		res.redirect('/register')
+	}
+	//	req.body.email
+	console.log(users)
+})
+
+
+app.delete('/logout', (req, res) => {
+	//	https://stackoverflow.com/questions/72336177/error-reqlogout-requires-a-callback-function
+	/* req.logOut()
+	res.redirect('/login') */
+	  req.logout(function(err) {
+		if (err) { return next(err); }
+		res.redirect('/');
+  });
+})
+
+
+function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next()
+	}
+	
+	res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return res.redirect('/')
+	}
+	
+	next()
+}
+
+module.exports = app;
